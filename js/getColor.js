fdc03b97062a6ade31f22d6dce8b962b47569285
alw231 <alexwang2017@u.northwestern.edu>
2014-04-13 12:11:01 -0500

    Icons

--- js/getColor.js
+++ js/getColor.js
@@ -2,7 +2,7 @@ $(function(){
 
 	var getColor = function()
 	{
-		var items = ['#CF8897','#D4BA39','#7D64BD','#DE5F77','#55B59B','#C4964B','#04D476','#4E4BA6','#47C1CC','#FF7052','#2D8A63','#A84A97','#3A6FBD'];
+		var items = ['#CF8897','#D4BA39','#7D64BD','#DE5F77','#55B59B','#C4964B','#04D476','#6E4682','#47C1CC','#FFAA8C','#2D8A63','#A84A97','#3A6FBD','#66CDAA','#FF6347','#483D8B'];
 		return(items[Math.floor(Math.random()*items.length)]);
 	}
 	
